91df3855f666fac4b0bb50cadfdf4a2e4e16939a
velican <120579492+ashborn211@users.noreply.github.com>
2025-09-15 15:23:12 +0200

    added all condiments

---
+++ Condiments/Liqour.cs
@@ -0,0 +1,27 @@
+﻿using DecoratorPattern.Condiments;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorPattern.Beverages
+{
+    internal class Liqour : CondimentDecorator
+    {
+        public Liqour(Beverage beverage)
+        {
+            baseBeverage = beverage;
+        }
+
+        public override double cost()
+        {
+            return 0.20 + baseBeverage.cost();
+        }
+
+        public override string GetDescription()
+        {
+            return baseBeverage.GetDescription() + ", Liqour ";
+        }
+    }
+}
---
+++ Condiments/White_chocolate.cs
@@ -0,0 +1,27 @@
+﻿using DecoratorPattern.Beverages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorPattern.Condiments
+{
+    internal class White_Chocolate : CondimentDecorator
+    {
+        public White_Chocolate(Beverage beverage)
+        {
+            this.baseBeverage = beverage;
+        }
+
+        public override double cost()
+        {
+            return 0.20 + baseBeverage.cost();
+        }
+
+        public override string GetDescription()
+        {
+            return baseBeverage.GetDescription() + ", White Chocolate ";
+        }
+    }
+}
