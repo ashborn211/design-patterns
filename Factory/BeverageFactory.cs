ee0e192f192b2827bd868445749a1865d014901c
velican <120579492+ashborn211@users.noreply.github.com>
2025-09-25 14:46:59 +0200

    added factory pattern

---
+++ Factory/BeverageFactory.cs
@@ -0,0 +1,219 @@
+﻿using DecoratorPattern.Beverages;
+using DecoratorPattern.Condiments;
+
+namespace DecoratorPattern.Factory
+{
+    internal static class BeverageFactory
+    {
+        public static Beverage Create(string beverageName, Size size = Size.TALL)
+        {
+            Beverage drink;
+
+            switch (beverageName.ToLower())
+            {
+                // 1. Espresso
+                case "espresso":
+                    drink = new Espresso();
+                    break;
+
+                // 2. Doppio (double/triple espresso)
+                case "doppio":
+                    drink = new Espresso();
+                    drink = new Espresso(drink);
+                    break;
+
+                // 3. Lungo
+                case "lungo":
+                    drink = new Espresso();
+                    drink = new Water(drink);
+                    break;
+
+                // 4. Ristretto
+                case "ristretto":
+                    drink = new Espresso();
+                    break;
+
+                // 5. Macchiato
+                case "macchiato":
+                    drink = new Espresso();
+                    drink = new Milk_Foam(drink);
+                    break;
+
+                // 6. Corretto
+                case "corretto":
+                    drink = new Espresso();
+                    drink = new Liqour(drink);
+                    break;
+
+                // 7. Con Panna
+                case "con panna":
+                    drink = new Espresso();
+                    drink = new Whip(drink);
+                    break;
+
+                // 8. Cappuccino
+                case "cappuccino":
+                    drink = new Espresso();
+                    drink = new Half_Milk(drink);
+                    drink = new Milk_Foam(drink);
+                    break;
+
+                // 9. Americano
+                case "americano":
+                    drink = new Espresso();
+                    drink = new Water(drink);
+                    drink = new Water(drink);
+                    break;
+
+                // 10. Caffè Latte
+                case "latte":
+                    drink = new Espresso();
+                    drink = new Half_Milk(drink);
+                    drink = new Milk_Foam(drink);
+                    break;
+
+                // 11. Flat White
+                case "flat white":
+                    drink = new Espresso();
+                    drink = new Half_Milk(drink);
+                    break;
+
+                // 12. Romano
+                case "romano":
+                    drink = new Espresso();
+                    drink = new Lemon(drink);
+                    break;
+
+                // 13. Marocchino
+                case "marocchino":
+                    drink = new Espresso();
+                    drink = new Chocolate(drink);
+                    drink = new Milk_Foam(drink);
+                    break;
+
+                // 14. Mocha
+                case "mocha":
+                    drink = new Espresso();
+                    drink = new Chocolate(drink);
+                    drink = new Half_Milk(drink);
+                    drink = new Whip(drink);
+                    break;
+
+                // 15. Bicerin
+                case "bicerin":
+                    drink = new Espresso();
+                    drink = new Chocolate(drink);
+                    drink = new Whip(drink);
+                    break;
+
+                // 16. Breve
+                case "breve":
+                    drink = new Espresso();
+                    drink = new Half_Milk(drink);
+                    drink = new Milk_Foam(drink);
+                    break;
+
+                // 17. Raf Coffee
+                case "raf":
+                    drink = new Espresso();
+                    drink = new Cream(drink);
+                    drink = new Vanilla_Sugar(drink);
+                    break;
+
+                // 18. Mead Raf
+                case "mead raf":
+                    drink = new Espresso();
+                    drink = new Cream(drink);
+                    drink = new Honey(drink);
+                    break;
+
+                // 19. Galao
+                case "galao":
+                    drink = new Espresso();
+                    drink = new Milk_Foam(drink);
+                    break;
+
+                // 20. Affogato
+                case "affogato":
+                    drink = new Espresso();
+                    drink = new Ice_cream(drink);
+                    break;
+
+                // 21. Vienna
+                case "vienna":
+                    drink = new Espresso();
+                    drink = new Whip(drink);
+                    break;
+
+                // 22. Glace
+                case "glace":
+                    drink = new Espresso();
+                    drink = new Ice_cream(drink);
+                    break;
+
+                // 23. Chocolate Milk
+                case "chocolate milk":
+                    drink = new Espresso();
+                    drink = new Half_Milk(drink);
+                    drink = new Black_chocolate(drink);
+                    break;
+
+                // 24. Demi-Crème
+                case "demi-crème":
+                    drink = new Espresso();
+                    drink = new Cream(drink);
+                    break;
+
+                // 25. Latte Macchiato
+                case "latte macchiato":
+                    drink = new Espresso();
+                    drink = new Half_Milk(drink);
+                    drink = new Milk_Foam(drink);
+                    break;
+
+                // 26. Freddo
+                case "freddo":
+                    drink = new Espresso();
+                    drink = new Liqour(drink);
+                    drink = new Ice(drink);
+                    break;
+
+                // 27. Frappuccino
+                case "frappuccino":
+                    drink = new Espresso();
+                    drink = new Half_Milk(drink);
+                    drink = new Whip(drink);
+                    break;
+
+                // 28. Caramel Frappuccino
+                case "caramel frappuccino":
+                    drink = new Espresso();
+                    drink = new Half_Milk(drink);
+                    drink = new Syrup(drink);
+                    drink = new Cream(drink);
+                    break;
+
+                // 29. Frappe
+                case "frappe":
+                    drink = new Espresso();
+                    drink = new Half_Milk(drink);
+                    drink = new Ice_cream(drink);
+                    break;
+
+                // 30. Irish Coffee
+                case "irish coffee":
+                    drink = new Espresso();
+                    drink = new Whiskey(drink);
+                    drink = new Whip(drink);
+                    break;
+
+                default:
+                    throw new ArgumentException($"Unknown beverage: {beverageName}");
+            }
+
+            // set size
+            drink.Size = size;
+            return drink;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿using DecoratorPattern.Beverages;
-using DecoratorPattern.Condiments;
+using DecoratorPattern.Factory;
 
 namespace DecoratorPattern
 {
@@ -7,189 +7,41 @@ namespace DecoratorPattern
     {
         static void Main(string[] args)
         {
-            // 1. Espresso
-            Beverage espresso = new Espresso();
-            espresso.Size = Size.TALL;
-            PrintBeverage(espresso);
-
-            // 2. Doppio 
-            Beverage doppio = new Espresso();
-            doppio = new Espresso();
-            doppio = new Espresso();
-            doppio.Size = Size.GRANDE;
-            PrintBeverage(doppio);
-
-            // 3. Lungo 
-            Beverage lungo = new Espresso();
-            lungo = new Water(lungo);
-            lungo.Size = Size.VENDI;
-            PrintBeverage(lungo);
-
-            // 4. Ristretto 
-            Beverage ristretto = new Espresso();
-            ristretto.Size = Size.TALL;
-            PrintBeverage(ristretto);
-
-            // 5. Macchiato
-            Beverage macchiato = new Espresso();
-            macchiato = new Milk_Foam(macchiato);
-            macchiato.Size = Size.GRANDE;
-            PrintBeverage(macchiato);
-
-            // 6. Corretto 
-            Beverage corretto = new Espresso();
-            corretto = new Liqour(corretto);
-            corretto.Size = Size.VENDI;
-            PrintBeverage(corretto);
-
-            // 7. Con Panna
-            Beverage conPanna = new Espresso();
-            conPanna = new Whip(conPanna);
-            PrintBeverage(conPanna);
-
-            // 8. Cappuccino 
-            Beverage cappuccino = new Espresso();
-            cappuccino = new Half_Milk(cappuccino);
-            cappuccino = new Milk_Foam(cappuccino);
-            cappuccino.Size = Size.GRANDE;
-            PrintBeverage(cappuccino);
-
-            // 9. Americano 
-            Beverage americano = new Espresso();
-            americano = new Water(americano);
-            americano = new Water(americano);
-            PrintBeverage(americano);
-
-            // 10. Caffè Latte 
-            Beverage latte = new Espresso();
-            latte = new Half_Milk(latte);
-            latte = new Milk_Foam(latte);
-            PrintBeverage(latte);
-
-            // 11. Flat White 
-            Beverage flatWhite = new Espresso();
-            flatWhite = new Half_Milk(flatWhite);
-            PrintBeverage(flatWhite);
-
-            // 12. Romano
-            Beverage romano = new Espresso();
-            romano = new Lemon(romano);
-            romano.Size = Size.TALL;
-            PrintBeverage(romano);
-
-            // 13. Marocchino
-            Beverage marocchino = new Espresso();
-            marocchino = new Chocolate(marocchino);
-            marocchino = new Milk_Foam(marocchino);
-            PrintBeverage(marocchino);
-
-            // 14. Mocha 
-            Beverage mocha = new Espresso();
-            mocha = new Chocolate(mocha);
-            mocha = new Half_Milk(mocha);
-            mocha = new Whip(mocha);
-            PrintBeverage(mocha);
-
-            // 15. Bicerin 
-            Beverage bicerin = new Espresso();
-            bicerin = new Chocolate(bicerin);
-            bicerin = new Whip(bicerin);
-            bicerin.Size = Size.VENDI;
-            PrintBeverage(bicerin);
-
-            // 16. Breve
-            Beverage breve = new Espresso();
-            breve = new Half_Milk(breve);
-            breve = new Milk_Foam(breve);
-            PrintBeverage(breve);
-
-            // 17. Raf Coffee 
-            Beverage raf = new Espresso();
-            raf = new Cream(raf);
-            raf = new Vanilla_Sugar(raf);
-            PrintBeverage(raf);
-
-            // 18. Mead Raf 
-            Beverage meadRaf = new Espresso();
-            meadRaf = new Cream(meadRaf);
-            meadRaf = new Honey(meadRaf);
-            meadRaf.Size = Size.TALL;
-            PrintBeverage(meadRaf);
-
-            // 19. Galao
-            Beverage galao = new Espresso();
-            galao = new Milk_Foam(galao);
-            PrintBeverage(galao);
-
-            // 20. Caffè Affogato
-            Beverage affogato = new Espresso();
-            affogato = new Ice_cream(affogato);
-            PrintBeverage(affogato);
-
-            // 21. Vienna Coffee
-            Beverage vienna = new Espresso();
-            vienna = new Whip(vienna);
-            vienna.Size = Size.GRANDE;
-            PrintBeverage(vienna);
-
-            // 22. Glace
-            Beverage glace = new Espresso();
-            glace = new Ice_cream(glace);
-            PrintBeverage(glace);
-
-            // 23. Chocolate Milk
-            Beverage chocolateMilk = new Espresso();
-            chocolateMilk = new Half_Milk(chocolateMilk);
-            chocolateMilk = new Black_chocolate(chocolateMilk);
-            PrintBeverage(chocolateMilk);
-
-            // 24. Demi-Crème
-            Beverage demiCreme = new Espresso();
-            demiCreme = new Cream(demiCreme);
-            PrintBeverage(demiCreme);
-
-            // 25. Latte Macchiato
-            Beverage latteMacchiato = new Espresso();
-            latteMacchiato = new Half_Milk(latteMacchiato);
-            latteMacchiato = new Milk_Foam(latteMacchiato);
-            PrintBeverage(latteMacchiato);
-
-            // 26. Freddo
-            Beverage freddo = new Espresso();
-            freddo = new Liqour(freddo);
-            freddo = new Ice(freddo);
-            freddo.Size = Size.TALL;
-            PrintBeverage(freddo);
-
-            // 27. Frappuccino
-            Beverage frappuccino = new Espresso();
-            frappuccino = new Half_Milk(frappuccino);
-            frappuccino = new Whip(frappuccino);
-            PrintBeverage(frappuccino);
-
-            // 28. Caramel Frappuccino
-            Beverage caramelFrappuccino = new Espresso();
-            caramelFrappuccino = new Half_Milk(caramelFrappuccino);
-            caramelFrappuccino = new Syrup(caramelFrappuccino);
-            caramelFrappuccino = new Cream(caramelFrappuccino);
-            PrintBeverage(caramelFrappuccino);
-
-            // 29. Frappe
-            Beverage frappe = new Espresso();
-            frappe = new Half_Milk(frappe);
-            frappe = new Ice_cream(frappe);
-            PrintBeverage(frappe);
-
-            // 30. Irish Coffee
-            Beverage irishCoffee = new Espresso();
-            irishCoffee = new Whiskey(irishCoffee);
-            irishCoffee = new Whip(irishCoffee);
-            PrintBeverage(irishCoffee);
+            PrintBeverage(BeverageFactory.Create("espresso", Size.TALL));
+            PrintBeverage(BeverageFactory.Create("doppio", Size.GRANDE));
+            PrintBeverage(BeverageFactory.Create("lungo", Size.VENDI));
+            PrintBeverage(BeverageFactory.Create("ristretto", Size.TALL));
+            PrintBeverage(BeverageFactory.Create("macchiato", Size.GRANDE));
+            PrintBeverage(BeverageFactory.Create("corretto", Size.VENDI));
+            PrintBeverage(BeverageFactory.Create("con panna"));
+            PrintBeverage(BeverageFactory.Create("cappuccino", Size.GRANDE));
+            PrintBeverage(BeverageFactory.Create("americano"));
+            PrintBeverage(BeverageFactory.Create("latte"));
+            PrintBeverage(BeverageFactory.Create("flat white"));
+            PrintBeverage(BeverageFactory.Create("romano", Size.TALL));
+            PrintBeverage(BeverageFactory.Create("marocchino"));
+            PrintBeverage(BeverageFactory.Create("mocha"));
+            PrintBeverage(BeverageFactory.Create("bicerin", Size.VENDI));
+            PrintBeverage(BeverageFactory.Create("breve"));
+            PrintBeverage(BeverageFactory.Create("raf"));
+            PrintBeverage(BeverageFactory.Create("mead raf", Size.TALL));
+            PrintBeverage(BeverageFactory.Create("galao"));
+            PrintBeverage(BeverageFactory.Create("affogato"));
+            PrintBeverage(BeverageFactory.Create("vienna", Size.GRANDE));
+            PrintBeverage(BeverageFactory.Create("glace"));
+            PrintBeverage(BeverageFactory.Create("chocolate milk"));
+            PrintBeverage(BeverageFactory.Create("demi-crème"));
+            PrintBeverage(BeverageFactory.Create("latte macchiato"));
+            PrintBeverage(BeverageFactory.Create("freddo", Size.TALL));
+            PrintBeverage(BeverageFactory.Create("frappuccino"));
+            PrintBeverage(BeverageFactory.Create("caramel frappuccino"));
+            PrintBeverage(BeverageFactory.Create("frappe"));
+            PrintBeverage(BeverageFactory.Create("irish coffee"));
         }
 
         static void PrintBeverage(Beverage beverage)
         {
-            Console.WriteLine(beverage.GetDescription()+" Size:"+ (beverage.Size) + " $" + beverage.cost().ToString("#.##"));
+            Console.WriteLine($"{beverage.GetDescription()} Size:{beverage.Size} ${beverage.cost():0.##}");
         }
     }
 }
